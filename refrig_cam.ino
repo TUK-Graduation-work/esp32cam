e97d451fce6a0ef82ee96d63f8de3aaf2c1271f4
chaecahe <108673522+chaecahe@users.noreply.github.com>
2023-07-16 22:16:12 +0900

    Add files via upload
    esp32cam연결, pan-tilt 움직이기

---
+++ refrig_cam.ino
@@ -0,0 +1,315 @@
+#define XCLK 20000000
+const char *ssid = "1413-2.4G";
+const char *password = "kpu123456!";
+
+#include "esp_camera.h"
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <iostream>
+#include <sstream>
+#include <ESP32Servo.h>
+
+String angle1 = "90";
+String angle2 = "90";
+String light_value = "0";
+
+#define PAN_PIN 14
+#define TILT_PIN 15
+
+Servo dummyServo1;
+Servo dummyServo2;
+Servo panServo;
+Servo tiltServo;
+
+//Camera related constants
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+AsyncWebServer server(80);
+
+#define LIGHT_PIN 4
+const int PWMLightChannel = 4;
+
+const char *htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <style>
+    .noselect {
+      -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+         -khtml-user-select: none; /* Konqueror HTML */
+           -moz-user-select: none; /* Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently
+                                      supported by Chrome and Opera */
+    }
+
+    .slidecontainer {
+      width: 100%;
+    }
+
+    .slider {
+      -webkit-appearance: none;
+      width: 100%;
+      height: 20px;
+      border-radius: 5px;
+      background: #d3d3d3;
+      outline: none;
+      opacity: 0.7;
+      -webkit-transition: .2s;
+      transition: opacity .2s;
+    }
+
+    .slider:hover {
+      opacity: 1;
+    }
+  
+    .slider::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background: red;
+      cursor: pointer;
+    }
+
+    .slider::-moz-range-thumb {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background: red;
+      cursor: pointer;
+    }
+
+    </style>
+  
+  </head>
+  <body class="noselect" align="center" style="background-color:white">
+     
+    <!--h2 style="color: teal;text-align:center;">Wi-Fi Camera &#128663; Control</h2-->
+    
+    <table id="mainTable" style="width:400px;margin:auto;table-layout:fixed" CELLSPACING=10>
+      <tr>
+        <img id="camera_image" src="/picture" style="width:400px;height:250px"></td>
+      </tr> 
+      <tr/><tr/>
+      <tr>
+        <td style="text-align:left"><b>Pan:</b></td>
+        <td colspan=2>
+         <div class="slidecontainer">
+            <input type="range" min="0" max="180" value="90" class="slider" id="Pan" oninput='sendButtonInput("Pan",value)'>
+          </div>
+        </td>
+      </tr> 
+      <tr/><tr/>       
+      <tr>
+        <td style="text-align:left"><b>Tilt:</b></td>
+        <td colspan=2>
+          <div class="slidecontainer">
+            <input type="range" min="0" max="180" value="90" class="slider" id="Tilt" oninput='sendButtonInput("Tilt",value)'>
+          </div>
+        </td>   
+      </tr>
+      <tr/><tr/>       
+      <tr>
+        <td style="text-align:left"><b>Light:</b></td>
+        <td colspan=2>
+          <div class="slidecontainer">
+            <input type="range" min="0" max="255" value="0" class="slider" id="Light" oninput='sendButtonInput("Light",value)'>
+          </div>
+        </td>   
+      </tr>      
+    </table>
+  
+    <script>
+      var xhttp = new XMLHttpRequest();
+
+      function init() 
+      {
+        var timestamp = new Date().getTime();
+        camera_image.src="/picture?t=" + timestamp;
+      }
+
+      function sendButtonInput(key, val) 
+      {
+        var data = key + "=" + val;
+        xhttp.open('GET', 'value?' + data, true); 
+        xhttp.send() 
+      }
+    
+      camera_image.onload = function (event) 
+      {
+        var timestamp = new Date().getTime();
+        camera_image.src="/picture?t=" + timestamp;
+      }
+
+      window.onload = init;
+    </script>
+  </body>    
+</html>
+)HTMLHOMEPAGE";
+
+void handleRoot(AsyncWebServerRequest *request)
+{
+  request->send_P(200, "text/html", htmlHomePage);
+}
+
+void handleNotFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "File Not Found");
+}
+
+void setupCamera()
+{
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = XCLK;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  config.fb_location = CAMERA_FB_IN_DRAM;
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void setUpPinModes()
+{
+  panServo.attach(PAN_PIN);
+  tiltServo.attach(TILT_PIN);
+
+  //Set up flash light
+  ledcSetup(PWMLightChannel, 1000, 8);
+  pinMode(LIGHT_PIN, OUTPUT);
+  ledcAttachPin(LIGHT_PIN, PWMLightChannel);
+}
+
+void setup(void)
+{
+  setUpPinModes();
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.onNotFound(handleNotFound);
+  server.on("/picture", HTTP_GET,
+            [](AsyncWebServerRequest *request)
+            {
+              camera_fb_t *frame = NULL;
+              frame = esp_camera_fb_get();
+              request->send_P(200, "image/jpeg", (const uint8_t *)frame->buf, frame->len);
+              esp_camera_fb_return(frame);
+            });
+  server.on("/value", HTTP_GET, value_handler);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  setupCamera();
+}
+
+void value_handler(AsyncWebServerRequest *request)
+{
+  if (request->hasParam("Light"))
+  {
+    light_value = request->getParam("Light")->value();
+    Serial.print("Light:");
+    Serial.println(light_value);
+  }
+  if (request->hasParam("Tilt"))
+  {
+    angle1 = request->getParam("Tilt")->value();
+    Serial.print("TILT:");
+    Serial.println(angle1);
+  }
+  if (request->hasParam("Pan"))
+  {
+    angle2 = request->getParam("Pan")->value();
+    Serial.print("PAN:");
+    Serial.println(angle2);
+  }
+  request->send(200, "text/plain", "");
+}
+
+void loop()
+{
+  int value1;
+  int value2;
+  int value3;
+  value1 = atoi(angle1.c_str());
+  value2 = atoi(angle2.c_str());
+  value3 = atoi(light_value.c_str());
+  panServo.write(value1);
+  tiltServo.write(value2);
+  ledcWrite(PWMLightChannel, value3);
+  Serial.print(" ");
+  Serial.print(value1);
+  Serial.print(" ");
+  Serial.print(value2);
+  Serial.print(" ");
+  Serial.print(value3);
+  Serial.println("");
+  delay(100);
+  //  wsCamera.cleanupClients();
+  // wsServoInput.cleanupClients();
+  //  sendCameraPicture();
+  //Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
+}
